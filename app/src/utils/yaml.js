bf424121d9763a56741c695fd56bc8842b742651
jpwarkentine <jonathan.warkentine@gmail.com>
2023-05-22 02:09:41 -0400

    requests.js seems to be sending requests

--- app/src/utils/yaml.js
+++ app/src/utils/yaml.js
@@ -2,14 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-const filePath = path.join(__dirname, '../tests.yml');
+const filePath = path.join(__dirname, '../../../tests.yml');
 
 let data;
 try {
   let fileContents = fs.readFileSync(filePath, 'utf8');
   data = yaml.load(fileContents);
 } catch (e) {
-  console.error(e);
+  console.error("Error parsing yaml tests file.", e);
 }
 
 module.exports = data;
