d78ae9dd9a70a8380b76ae8744b7c425be065099
jpwarkentine <jonathan.warkentine@gmail.com>
2023-05-22 15:44:04 -0400

    going to pivot to doing this project in Go

--- app/src/index.js
+++ app/src/index.js
@@ -1,19 +1,4 @@
 const tests = require('./utils/requests');
 const generateTests = require('./utils/generateTests');
 
-let mochaTestSuite;
-
-// gather all API call requests into a single flattened array
-let allTestRequests = [];
-tests.forEach(group => {
-    allTestRequests.push(group.map(test => test.request));
-});
-
-// funnel gathered requests in a single promise
-Promise.all(allTestRequests)
-    .then( () => {
-        mochaTestSuite = generateTests(tests);
-    })
-    .catch(err => {
-        console.error(err);
-      });
+generateTests(tests);
